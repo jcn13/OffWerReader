35793f7a30b64a106624062d58e86d76bed54b96
jcn13 <jcn13@uol.com.br>
2016-10-05 18:33:55 -0300

    no message

---
+++ scripts/Scraping.js
@@ -0,0 +1,313 @@
+/*eslint-env browser */
+/*eslint no-var: "error"*/
+/*eslint prefer-const: "error"*/
+/*eslint-env es6*/
+
+const btnScrape = document.querySelector("#btn-scrape");
+const inputScrape = document.querySelector("#input-scrape");
+// const indexedDbShowButton = document.querySelector("#indexedDbShow");
+const resultsAnchor = document.querySelector('#resultsAnchor');
+const nextChapterLink = document.querySelector('.next');
+const previousChapterLink = document.querySelector('.prev');
+
+let chaptersTotal = document.querySelector('#chapters-total');
+
+let Story = {};
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    openDb(function() {
+        updateStoryList();
+    });
+});
+
+btnScrape.addEventListener('click', StartScrap);
+
+nextChapterLink.addEventListener('click', function(e) {
+    if (this.classList.contains('disable'))
+        return;
+
+    Story.currentChapter += 1;
+    getCurrentChapter();
+    updateNav();
+    e.preventDefault();
+});
+
+previousChapterLink.addEventListener('click', function(e) {
+    if (this.classList.contains('disable'))
+        return;
+
+    if (Story.currentChapter > 1) {
+        Story.currentChapter -= 1;
+        getCurrentChapter();
+        updateNav();
+    }
+    e.preventDefault();
+});
+
+function updateNav() {
+    var chaptersSelect = document.querySelector('#chapters-select');
+    chaptersSelect.selectedIndex = Story.currentChapter - 1;
+    if (Story.currentChapter > 1) {
+        previousChapterLink.classList.remove('disable');
+
+        if (Story.currentChapter == Story.chapters) {
+            nextChapterLink.classList.add('disable');
+        } else {
+            nextChapterLink.classList.remove('disable');
+        }
+    } else if (Story.currentChapter == 1) {
+        previousChapterLink.classList.add('disable');
+        if (Story.chapters > 1) {
+            nextChapterLink.classList.remove('disable');
+        }
+    }
+}
+
+function StartScrap(e) {
+    const parsedInput = parseUserInput(inputScrape.value, supportedSites);
+    const yqlStringLinks = yqlStringBuilder(parsedInput.href, parsedInput.xpathLinks);
+    const yqlStringChapters = new Set();
+    var title = document.querySelector('#title');
+
+    //if auth, if f1 => createStoryFolderGDrive
+    listFilesFolder(parsedInput.storyId, id, function(idc)
+    {
+        Story.name = parsedInput.storyName;
+        title.textContent = Story.name;
+        makeRequest('GET', yqlStringLinks).then(function(data) 
+        {
+            var numberOfChapters = (JSON.parse(data)).query.results.select[0].option.length;
+            chaptersTotal.textContent = numberOfChapters;
+
+            Story.chapters = numberOfChapters;
+            Story.data = data;
+            Story.parsedInput = parsedInput;
+            Story.currentChapter = 1;
+            Story.id = parsedInput.storyId;
+            Story.href = parsedInput.href;
+
+            populateChaptersSelectOptions();
+            populateChapters();
+
+        }).catch(function(err) {
+            console.log('Request failed', err);
+        })
+    });
+   
+}
+
+function populateChaptersSelectOptions() {
+    var chaptersSelect = document.querySelector('#chapters-select');
+    for (var i = 1; i <= Story.chapters; i++) {
+        var opt = document.createElement("option");
+        opt.value = i;
+        opt.innerHTML = "Chapter: " + i;
+
+        chaptersSelect.appendChild(opt);
+    }
+
+    chaptersSelect.addEventListener('change', function() {
+        goToChapter(this.value);
+    })
+}
+
+function populateChapters() {
+    for (var i = 1; i <= 3; i++) {
+        const url = Story.parsedInput.hrefEmptyChapter + i,
+            xpath = Story.parsedInput.xpathStory;
+
+        const nextStoryPath = Story.id + "." + i;
+        makeRequest('GET', yqlStringBuilder(url, xpath, 'xml'))
+            .then(function(data) {
+                addOrReplaceStory(nextStoryPath, Story.name, Story.href,
+                    data, Story.chapters);
+                updateStoryList();
+                    //if f1, f2, auth => sendChapterObjectToGoogleDrive
+                    const obj = {
+                        "ChapterId": nextStoryPath,
+                        "StoryName": Story.name,
+                        "Url": Story.href,
+                        "Content": data,
+                        "NumberOfChapters": Story.chapters
+                    };                    
+                    insertFile(nextStoryPath, idStory, obj);
+            })
+            .catch(function(err) {
+                console.log('Request failed', err);
+            })
+    }
+
+    getCurrentChapter();
+}
+
+function updateStoryList() {
+    populateStoryArray(function(data){
+        const strList = document.querySelector(".sidebar-list");
+        strList.innerHTML = '';
+        data.forEach(function(obj, i) {
+          strList.insertAdjacentHTML('beforeend', `
+            <a href="#" class="sidebar-list--item story-sel" data-story="${i}" title="${obj.StoryName}">
+                <span class="sidebar-list--text">${obj.StoryName} - ${obj.NumberOfChapters} chapters</span>
+            </a>`);
+        });
+
+        const storySelector = document.querySelectorAll('.story-sel');
+        for (var i = storySelector.length - 1; i >= 0; i--) {
+            storySelector[i].addEventListener('click', function(e) {
+                console.log(this.dataset.story);
+                var s = this.dataset.story;
+
+                Story.name = data[s].StoryName;
+                Story.id = data[s].ChapterId.split(".")[0];
+                Story.chapters = data[s].NumberOfChapters;
+                chaptersTotal.textContent = Story.chapters;
+                title.textContent = Story.name;
+                Story.currentChapter = 1;
+                getCurrentChapter();
+                updateNav();
+                populateChaptersSelectOptions();
+            });
+        }
+    });
+}
+
+function goToChapter(chapter) {
+    Story.currentChapter = chapter;
+    updateNav();
+    getCurrentChapter();
+}
+
+function getCurrentChapter() {
+    const nextStoryPath = Story.id + "." + Story.currentChapter;
+    getChapter(nextStoryPath);
+}
+
+// indexedDbShowButton.addEventListener("click", function(){
+//     populateStoryArray(function (data){ //TODO: Raphael, passar o callback aqui para montar o menu lateral?
+//         // mas não em um click né, tem que fazer isso depois que a conexão com DB tiver aberto de fato.
+//         data.forEach(function(obj) {
+//           storyList.insertAdjacentHTML('beforeend', `<div class="chapterBox">${obj.StoryName}</div>`);
+//         });
+//   });
+//     //displayStoryList(getObjectStore(DB_STORE_NAME, 'readwrite'));
+// });
+
+const supportedSites = new Map([
+    ["www.fanfiction.net", {
+        xpathLinks: '//*[@id="chap_select"]',
+        xpathStory: '//*[@id="storytext"]',
+        jsonNChapters: '.query.results.select[0].option.length'
+    }],
+    ["m.fanfiction.net", {
+        xpathLinks: '//*[@id="jump"]',
+        xpathStory: '//*[@id="storytext"]',
+        jsonNChapters: '.query.results.select[0].option.length'
+    }],
+    ["www.fictionpress.com", {
+        xpathLinks: '//*[@id="chap_select"]',
+        xpathStory: '//*[@id="storytext"]',
+        jsonNChapters: '.query.results.select[0].option.length'
+    }],
+    ["m.fictionpress.com", {
+        xpathLinks: '//*[@id="d_menu"]/div/form',
+        xpathStory: '//*[@id="storytext"]',
+        jsonNChapters: '.query.results.select[0].option.length'
+    }],
+]);
+
+function parseUserInput(url, supSites) {
+    if (!url) {
+        console.log(`Couldn't find url to be parsed`);
+        return;
+    }
+    const input = parseUrl(url);
+    if (!supSites.has(input.hostname)) {
+        console.log(`I'm sorry, '${input.value}' not found in our supported sites list`);
+        return;
+    }
+    input.xpathLinks = supSites.get(input.hostname).xpathLinks;
+    input.xpathStory = supSites.get(input.hostname).xpathStory;
+    if (!input.xpathLinks || !input.xpathStory) {
+        console.log(`parseUserInput input problem:
+                  xpathLinks: ${input.xpathLinks}
+                  xpathStory: ${input.xpathStory}`);
+        return;
+    }
+    console.log(`Site ${input.name} successfully detected`);
+    console.log(JSON.stringify(input, undefined, 2));
+    return input;
+}
+
+function yqlStringBuilder(parsedUrl, xpath, format = 'json') {
+    if (!parsedUrl || !xpath) {
+        console.log(`yqlStringBuilder input problem:
+                      parsedUrl: ${parsedUrl}
+                      xpath: ${xpath}`);
+        return;
+    }
+    const yql = 'https://query.yahooapis.com/v1/public/yql?' + 'q=' + encodeURIComponent(`select * from html where url=@url and xpath='${xpath}'`) + '&url=' + encodeURIComponent(parsedUrl) + `&crossProduct=optimized&format=${format}`;
+
+    if (!yql) {
+        console.log(`something went wrong while building yqlString:
+                      yqlQueryString: ${yql}
+                      yqlQuery: ${yqlQuery}`);
+    }
+    return yql;
+}
+
+function makeRequest(method, url) {
+    return new Promise(function(resolve, reject) {
+        const xhr = new XMLHttpRequest();
+        console.log(`making request with url: ${url}`);
+        xhr.open(method, url);
+        xhr.onload = function() {
+            if (this.status >= 200 && this.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function() {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
+}
+
+const parseUrl = (function() {
+    const a = document.createElement('a');
+    return function(url) {
+        a.href = url;
+        hostArrDot = a.host.split('.');
+        hrefArrSlash = a.href.split('/');
+        if (!hostArrDot[0] || !hostArrDot[1]) {
+            console.log(`There's a problem in the story link`);
+        }
+        if (!hrefArrSlash[4]) {
+            console.log(`Story ID could not be parsed from link`);
+        }
+        return {
+            origin: a.origin,
+            host: a.host,
+            href: a.href,
+            hostname: a.hostname,
+            pathname: a.pathname,
+            port: a.port,
+            protocol: a.protocol,
+            search: a.search,
+            hash: a.hash,
+            xpathLinks: '',
+            xpathStory: '',
+            name: hostArrDot[0] == 'www' || hostArrDot[0] == 'm' ? hostArrDot[1] : hostArrDot[0],
+            hrefEmptyChapter: a.origin + `/s/${hrefArrSlash[4]}/`,
+            storyId: hrefArrSlash[4],
+            storyName: hrefArrSlash[6]
+        };
+    }
+})();
---
+++ scripts/gapi-auth.js
@@ -0,0 +1,176 @@
+/*eslint-env browser */
+/*eslint no-var: "error"*/
+/*eslint prefer-const: "error"*/
+/*eslint-env es6*/
+
+      var CLIENT_ID = '698825936465-j1cs44897v5flnfrf7fpppnukp6okpq7.apps.googleusercontent.com';
+      var SCOPES = 'https://www.googleapis.com/auth/drive';
+      var id = null; 
+      var idOff = null;      
+      var idStory = undefined; 
+      var storyName = undefined;          
+      /**
+       * Called when the client library is loaded to start the auth w.
+       */
+      function handleClientLoad() {
+        window.setTimeout(checkAuth, 1);
+      }
+      /**
+       * Check if the current user has authorized the application.
+       */
+      function checkAuth() {
+        gapi.auth.authorize(
+            {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': true},
+            handleAuthResult);
+            
+      }
+      /**
+       * Called when authorization server replies.
+       *
+       * @param {Object} authResult Authorization result.
+       */
+      function handleAuthResult(authResult) {
+        var authButton = document.getElementById('authorizeButton');        
+        authButton.style.display = 'none';        
+        if (authResult && !authResult.error) {
+          // Access token has been successfully retrieved, requests can be sent to the API.
+          authButton.style.display = 'none';
+          gapi.client.load('drive', 'v2', function() {listFilesFolder("OffWebReader");});;           
+        } else {
+          // No access token could be retrieved, show the button to start the authorization flow.
+          authButton.style.display = 'block';
+          authButton.onclick = function() {
+              gapi.auth.authorize(
+                  {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': false},
+                  handleAuthResult);
+          };
+        }
+      }
+      /**
+       * Start the file upload.
+       *
+       * @param {Object} evt Arguments from the file selector.
+       */
+      function uploadFile(evt) {
+        gapi.client.load('drive', 'v2', function() {
+          //listFiles("OffWebReader", "TesteMagico.2", appState);
+          listFilesFolder("OffWebReader", id, null);           
+          //setTimeout(function(){ listFiles(storyName, "TesteMagico.4", appState); }, 4000);
+          //setTimeout(function(){ deleteFile(idStory); }, 7000);                        
+        });
+      }
+      /**
+       * Insert new file.
+       */
+      function deleteFile(folderStory, callback) 
+      {
+        var request = gapi.client.drive.files.delete({
+        'fileId': folderStory        
+        });      
+      request.execute(function(resp) { });      
+      }
+     function insertFile(name, id, obj) {        
+        const boundary = '-------314159265358979323846264';
+        const delimiter = "\r\n--" + boundary + "\r\n";
+        const close_delim = "\r\n--" + boundary + "--";        
+        var fileName = name;
+        var contentType = 'application/json';
+        var parentId = id
+        var metadata = {          
+          'title': fileName,
+          'mimeType': contentType,
+          "parents": [{"id":parentId}]
+        };
+        var a = (JSON.stringify(obj));        
+        var multipartRequestBody =
+            delimiter +
+            'Content-Type: application/json\r\n\r\n' +
+            JSON.stringify(metadata) +
+            delimiter +
+            'Content-Type: ' + contentType + '\r\n\r\n' +
+            a +
+            close_delim;
+        var request = gapi.client.request({
+            'path': '/upload/drive/v2/files',
+            'method': 'POST',
+            'params': {'uploadType': 'multipart'},
+            'headers': {
+              'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
+            },
+            'body': multipartRequestBody});
+        request.execute(function(arg) {
+          console.log(arg);
+          fileId=arg.id;
+          console.log(fileId);          
+        });                          
+      }      
+    function createFolder(name, parent, callback)
+    {
+  	  var pai = parent;
+      data = new Object();
+      data.title = name;
+      if(pai != undefined)
+      {
+          data.parents = [{"id":pai}];
+      }
+      data.mimeType = "application/vnd.google-apps.folder";                 
+  		gapi.client.drive.files.insert({'resource': data}).execute(function(fileList)
+      {
+        if(name == "OffWebReader")
+        {
+          idOff = fileList.id;
+          if (callback != null)
+          {
+            return callback(id);
+          }                          
+        }
+        else
+        {
+          console.log(fileList);
+          idStory = fileList.id;
+          return callback(idStory); 
+        }
+      });         
+      console.log("folder: " + name + " criado"); 
+    }	   
+     function listFilesFolder(folderName, parent, callback) 
+   {
+        var request = gapi.client.drive.files.list({
+            //'parents': [{"isRoot":true}],
+            'mimeType':"application/vnd.google-apps.folder"
+          });
+          request.execute(function(resp)
+          {
+            count = 0;
+            var files = resp.items;            
+            if (files && files.length > 0) 
+            {
+              for (var i = 0; i < files.length; i++) 
+              {
+                file = files[i];                
+                id = file.id;
+                var title = file.title;                             
+                console.log(id+" / "+title);                
+                if(title == folderName)
+                {                
+                  count++;                  
+                  if(folderName != "OffWebReader")
+                  {
+                    id = idStory;                          
+                    return callback(idStory);                    
+                  }
+                  else
+                  {
+                    idOff = id;
+                  }
+                  return console.log("folder"+ folderName + " existe");                 
+                }                                                
+              }
+            }
+            if (count == 0)
+            {
+              createFolder(folderName, id, callback);              
+              count++;                         
+            }  
+          });                                    
+    }      
